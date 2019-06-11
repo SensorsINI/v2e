04294e6ecaa4f1d1f2fb1e575907a6dc88fef871
Zhe He <hezhehz@live.cn>
2019-06-11 18:01:32 +0200

    argparser

--- test/reader_test.py
+++ test/reader_test.py
@@ -1,15 +1,21 @@
 import sys
-import queue as Queue
-import pdb
-import time
+import argparse
 
 sys.path.append("../")
 sys.path.append("../utils")
 
 from src.reader import Reader
 
-fname = "../data/rec1487354811.hdf5"
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--fname",
+    type=str,
+    help="path of the input .hdf5 file")
+
+args = parser.parse_args()
+
+fname = args.fname
 
 m = Reader(fname, start=5)
 frames, events = m.read()
-pdb.set_trace()
