d9fd19e02838d65d51c92381bf3054d1bb5bc511
Zhe He <hezhehz@live.cn>
2019-07-04 22:38:54 +0200

    video2events

---
+++ test/video2events.py
@@ -0,0 +1,126 @@
+"""
+Python code for extracting frames from video and converting them into DVS
+events.
+
+@author: Zhe He
+@contact: zhehe@student.ethz.ch
+@latest update: 2019-Jul-4th
+"""
+
+
+import argparse
+import cv2
+import numpy as np
+import sys
+import os
+
+from tempfile import TemporaryDirectory
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--input",
+    type=str,
+    required=True,
+    help="input video"
+)
+
+parser.add_argument(
+    "--pos_thres",
+    type=float,
+    default=0.21,
+    help="threshold to trigger a positive event"
+)
+
+parser.add_argument(
+    "--neg_thres",
+    type=float,
+    default=0.17,
+    help="threshold to trigger a negative event"
+)
+
+parser.add_argument(
+    "--sf",
+    type=int,
+    required=True,
+    help="slow motion factor"
+)
+parser.add_argument(
+    "--frame_rate",
+    type=int,
+    help="frame rate of output video"
+)
+
+parser.add_argument(
+    "--video_path",
+    type=str,
+    required=True,
+    help="path to store output vidoes"
+)
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    sys.path.append("../")
+    sys.path.append("../src/")
+    sys.path.append("../utils/")
+
+    from src.renderer import RenderFromImages
+    from src.slomo import SuperSloMo
+
+    frames = []
+
+    cap = cv2.VideoCapture(args.input)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            # convert RGB frame into luminance.
+            frame = (0.02126 * frame[:, :, 0] +
+                     0.7152 * frame[:, :, 1] +
+                     0.0722 * frame[:, :, 2])
+            frames.append(frame)
+        else:
+            break
+    cap.release()
+    frames = np.stack(frames)
+    input_ts = output_ts = np.arange(
+        0,
+        frames.shape[0] / fps,
+        1 / fps
+    )
+
+    output_ts = np.arange(
+        0,
+        frames.shape[0] / fps,
+        1 / args.frame_rate
+    )
+
+    with TemporaryDirectory() as dirname:
+
+        print("tmp_dir: ", dirname)
+
+        s = SuperSloMo(
+            args.checkpoint,
+            args.sf,
+            dirname,
+            video_path=args.video_path
+        )
+        s.interpolate(frames)
+        interpolated_ts = s.get_ts(input_ts)
+        height, width = frames.shape[1:]
+
+        r = RenderFromImages(
+            dirname,
+            output_ts,
+            interpolated_ts,
+            args.pos_thres,
+            args.neg_thres,
+            os.path.join(args.video_path, "from_image.avi"),
+            rotate=True)
+
+        _, _, _ = r.render(height, width)
