2b3d204089c05ac91064feb3040e32d9ea6b8b9b
Yuhuang Hu <duguyue100@gmail.com>
2019-10-08 14:47:28 +0200

    sketch for ilsvrc dataset exporting

---
+++ ilsvrc-dataset.py
@@ -0,0 +1,162 @@
+"""Script to generate ILSVRC video object detection dataset.
+
+Author: Yuhuang Hu
+Email : yuhuang.hu@ini.uzh.ch
+"""
+
+from __future__ import print_function, absolute_import
+
+import argparse
+import numpy as np
+import os
+import shutil
+import glob
+from skimage.io import imread
+
+from tempfile import TemporaryDirectory
+
+from src.renderer import RenderFromImages, RenderFromArray
+from src.slomo import SuperSloMo
+
+# define a parser
+parser = argparse.ArgumentParser()
+
+# root folder for either train or val partition
+parser.add_argument("--dir", "-d", type=str)
+parser.add_argument("--out", "-o", type=str)
+
+parser.add_argument(
+    "--pos_thres",
+    type=float,
+    default=0.25,
+    help="threshold to trigger a positive event"
+)
+
+parser.add_argument(
+    "--neg_thres",
+    type=float,
+    default=0.35,
+    help="threshold to trigger a negative event"
+)
+
+parser.add_argument(
+    "--sf",
+    type=int,
+    required=True,
+    help="slow motion factor"
+)
+
+parser.add_argument(
+    "--checkpoint",
+    type=str,
+    required=True,
+    help="path of checkpoint"
+)
+
+args = parser.parse_args()
+
+# set fps, use 30
+fps = 30.
+
+assert os.path.isdir(args.dir)
+
+if not os.path.isdir(args.out):
+    os.makedirs(args.out)
+
+# get the list of directory
+collectd_paths = []
+for root, dirs, files in os.walk(args.dir):
+    if len(dirs) == 0:
+        collectd_paths.append(root)
+
+for vid_path in collectd_paths:
+    # set up output folder
+    base_name = os.path.basename(vid_path)
+    vid_out_path = os.path.join(args.out, base_name)
+    if not os.path.isdir(vid_out_path):
+        os.makedirs(vid_out_path)
+
+    # get all frames
+    file_list = sorted(glob.glob("{}".format(vid_path)+"/*.*"))
+
+    frames = []
+
+    for img_file in file_list:
+        # read image
+        frame = imread(img_file)
+
+        if frame.ndim == 3:
+            # convert image
+            frame = (0.2126 * frame[:, :, 0] +
+                     0.7152 * frame[:, :, 1] +
+                     0.0722 * frame[:, :, 2])
+
+        frame = frame.astype(np.uint8)
+
+        frames.append(frame)
+        print("Loading file {}".format(img_file))
+
+    frames = np.stack(frames)
+    num_frames = frames.shape[0]
+
+    # this is in seconds
+    input_ts = output_ts = np.linspace(
+        0,
+        num_frames / fps,
+        num_frames,
+        endpoint=False
+    )
+
+    # export frame time stamps
+    np.save(os.path.join(vid_out_path, "frame_ts.npy"), input_ts)
+
+    with TemporaryDirectory() as dirname:
+        print("tmp_dir: ", dirname)
+
+        s = SuperSloMo(
+            args.checkpoint,
+            args.sf,
+            dirname,
+            video_path=vid_out_path
+        )
+        s.interpolate(frames)
+        interpolated_ts = s.get_ts(input_ts)
+        height, width = frames.shape[1:]
+
+        for factor in [1, args.sf]:
+            output_ts = np.linspace(
+                0,
+                (num_frames - 1) / fps,
+                factor * (num_frames - 1),
+                endpoint=False
+            )
+
+            r_slomo = RenderFromImages(
+                dirname,
+                output_ts,
+                interpolated_ts,
+                args.pos_thres,
+                args.neg_thres,
+                os.path.join(
+                    vid_out_path,
+                    "interpolated_{:d}.avi".format(int(factor * fps))
+                )
+            )
+
+            r_input = RenderFromArray(
+                frames,
+                output_ts,
+                input_ts,
+                args.pos_thres,
+                args.neg_thres,
+                os.path.join(
+                    vid_out_path,
+                    "input_{:d}.avi".format(int(factor * fps))
+                )
+            )
+
+            _ = r_slomo.render(height, width)
+            _ = r_input.render(height, width)
+
+        #  shutil.copy2(args.input, args.output_dir)
+    break
