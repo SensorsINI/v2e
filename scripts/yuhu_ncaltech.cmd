1a32d23ccc02dcd980c05eac9e21f4f810abe163
tobidelbruck <todelbru@ethz.ch>
2021-08-04 10:43:40 +0200

    moved scripts to a folder

---
+++ scripts/yuhu_ncaltech.cmd
@@ -0,0 +1,1 @@
+python v2e.py -i input\ncaltech_videos_for_tobi\image_0001.mp4 --overwrite --output_in_place --no_preview --pos_thres 0.3 --neg_thres 0.3 --sigma_thres 0.03 --cutoff_hz 0 --leak_rate_hz 0.5 --shot_noise_rate_hz 0 --input_frame_rate 30 --dvs_h5 v2e.h5 --dvs_aedat2 v2e.aedat --dvs_text None  --auto_timestamp_resolution true --skip_video_output --dvs_exposure duration 0.03
